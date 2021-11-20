93bbc928887e0172b8e45f70dc33f99ea7ee6f79
Rido Ananda <ridoananda123@gmail.com>
2021-11-20 16:45:29 +0700

    fill the sidebar menu and adding post page

--- pages/question/index.vue
+++ pages/question/index.vue
@@ -1,18 +1,26 @@
-<v-card>
-      <v-card-title>
-        <v-text-field
-          v-model="search"
-          append-icon="mdi-magnify"
-          label="Search"
-          single-line
-          hide-details
-        ></v-text-field>
-      </v-card-title>
-      <v-data-table
-        :headers="headers"
-        :items="desserts"
-        :items-per-page="5"
-        :search="search"
-        class="elevation-1"
-      ></v-data-table>
-    </v-card>
+<template>
+  <v-card>
+    <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :items-per-page="5"
+      :search="search"
+      class="elevation-1"
+    ></v-data-table>
+  </v-card>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
